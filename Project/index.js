4e397299acf954b0da7fe3961b5a593d98f8ecdf
leescofiel <92999862+leescofiel@users.noreply.github.com>
2023-04-15 15:59:41 +0530

    Add files via upload

---
+++ Project/index.js
@@ -0,0 +1,12 @@
+const login = document.querySelector(".btn-success");
+const addwallet = document.querySelector(".btn-primary");
+
+login.addEventListener("click", function () {
+  window.location =
+    "file:///C:/Users/259813/Documents/Udemy%20k=js/Project/Login.html";
+});
+
+addwallet.addEventListener("click", function () {
+  window.location =
+    "file:///C:/Users/259813/Documents/Udemy%20k=js/Project/Addwalet.html";
+});
